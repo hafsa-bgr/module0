5e20ec241fe8b73619ddc4b93a6b8da735d0ab36
hafsa-bgr <hafsa.boughir@gmail.com>
2025-02-18 11:21:32 +0100

    definitieve versie

--- external /index.js
+++ external /index.js
@@ -1,5 +1,5 @@
 document.addEventListener ("DOMContentLoaded", function(){
-    const knop=document.getElementById ("begroeting");
+    const knop=document.getElementById ("Hallo");
 
     knop.addEventListener ("click", function(){
         this.innerText = this.innerText === "Hallo!" ? "Tot ziens!" : "Hallo!";
